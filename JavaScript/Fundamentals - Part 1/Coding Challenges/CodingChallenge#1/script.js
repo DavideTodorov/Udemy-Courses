94fd034e3df70d4970ae1ea45ae3a426fc1f6ce5
Davide Todorov <davide.todorov@abv.bg>
2020-12-25 19:57:42 +0200

    Refactor CodingChallenge#1

--- JavaScript/Fundamentals - Part 1/Coding Challenges/CodingChallenge#1/script.js
+++ JavaScript/Fundamentals - Part 1/Coding Challenges/CodingChallenge#1/script.js
@@ -6,6 +6,8 @@ const johnsHeight = 1.95;
 
 const marksBMI = marksWeight / marksHeight ** 2;
 const johnsBMI = johnsWeight / johnsHeight ** 2;
+const isMarksBMIHigher = marksBMI > johnsBMI;
 
 console.log("Mark's BMI: " + marksBMI);
 console.log("John's BMI: " + johnsBMI);
+console.log("Is Mark's BMI higher? " + isMarksBMIHigher);
