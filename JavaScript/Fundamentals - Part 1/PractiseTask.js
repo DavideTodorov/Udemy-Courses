92e759170d4ff75b19048d9a901e98a2d0c7ba81
Davide Todorov <davide.todorov@abv.bg>
2020-12-25 08:43:13 +0200

    Refactor code and finished task_03

---
+++ JavaScript/Fundamentals - Part 1/PractiseTask.js
@@ -0,0 +1,11 @@
+const country = "Bulgaria";
+const continent = "Europe";
+let population = 6000000;
+const isIsland = false;
+const language = "Bulgarian";
+
+console.log("Country name: " + country);
+console.log("Continent name: " + continent);
+console.log("Population: " + population);
+console.log("Is island: " + isIsland);
+console.log("Language: " + language);
