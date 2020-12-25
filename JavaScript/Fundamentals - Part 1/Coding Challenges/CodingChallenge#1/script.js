180021a91fc090f549bc61d263f696546cc8c1fe
Davide Todorov <davide.todorov@abv.bg>
2020-12-25 11:47:36 +0200

    Finished CodingChallenge#1

---
+++ JavaScript/Fundamentals - Part 1/Coding Challenges/CodingChallenge#1/script.js
@@ -0,0 +1,11 @@
+const marksWeight = 78;
+const marksHeight = 1.69;
+
+const johnsWeight = 92;
+const johnsHeight = 1.95;
+
+const marksBMI = marksWeight / marksHeight ** 2;
+const johnsBMI = johnsWeight / johnsHeight ** 2;
+
+console.log("Mark's BMI: " + marksBMI);
+console.log("John's BMI: " + johnsBMI);
