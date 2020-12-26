6aa7238ea34c133fa2c913dd35e000258abdde28
Davide Todorov <davide.todorov@abv.bg>
2020-12-26 08:13:40 +0200

    Finished If/Else statement task

--- JavaScript/Fundamentals - Part 1/MainTask/PractiseTask.js
+++ JavaScript/Fundamentals - Part 1/MainTask/PractiseTask.js
@@ -14,5 +14,18 @@ console.log("Half of the population: " + population / 2);
 console.log("Does my country have bigger population than Finland? ", population > 6000000);
 console.log("Does my country has less than average population? ", population < 33000000);
 
-const coutryDescription = country + " is in " + continent + " and its " + population + " people speak " + language;
+const coutryDescription = `${country} is in ${continent}, and its ${population} people speak ${language}`;
 console.log(coutryDescription);
+
+
+let populationMessage;
+const averagePopulation = 33000000;
+
+if (population > averagePopulation) {
+    populationMessage = `${country}'s population is above average.`;
+} else {
+    const populationBelowAverage = averagePopulation - population;
+    populationMessage = `${country}'s population is ${populationBelowAverage} below average.`;
+}
+
+console.log(populationMessage);
