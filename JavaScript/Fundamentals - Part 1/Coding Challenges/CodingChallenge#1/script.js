d619240a4a341ea8642641d4fed3db3b91940c05
Davide Todorov <davide.todorov@abv.bg>
2020-12-26 08:21:01 +0200

    Finished CodingChallenge#2

--- JavaScript/Fundamentals - Part 1/Coding Challenges/CodingChallenge#1/script.js
+++ JavaScript/Fundamentals - Part 1/Coding Challenges/CodingChallenge#1/script.js
@@ -8,6 +8,15 @@ const marksBMI = marksWeight / marksHeight ** 2;
 const johnsBMI = johnsWeight / johnsHeight ** 2;
 const isMarksBMIHigher = marksBMI > johnsBMI;
 
-console.log(`Mark's BMI: ${marksBMI.toFixed(3)}`);
-console.log(`John's BMI: ${johnsBMI.toFixed(3)}`);
-console.log("Is Mark's BMI higher? " + isMarksBMIHigher);
+console.log(`Mark's BMI: ${marksBMI.toFixed(2)}`);
+console.log(`John's BMI: ${johnsBMI.toFixed(2)}`);
+
+let higherBMIMessage;
+
+if (marksBMI > johnsBMI) {
+    higherBMIMessage = `Mark's BMI (${marksBMI.toFixed(1)}) is higher than John's BMI (${johnsBMI.toFixed(1)})!`;
+}else{
+    higherBMIMessage = `John's BMI (${johnsBMI.toFixed(1)}) is higher than Mark's BMI (${marksBMI.toFixed(1)})!`;
+}
+
+console.log(higherBMIMessage);
