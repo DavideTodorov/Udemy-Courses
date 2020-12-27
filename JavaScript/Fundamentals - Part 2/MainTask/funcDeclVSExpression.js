f4705e0e8e62101fbf9e5dcd844886a6974807d6
Davide Todorov <davide.todorov@abv.bg>
2020-12-27 18:09:13 +0200

    Finished multi functions calls task

--- JavaScript/Fundamentals - Part 2/MainTask/funcDeclVSExpression.js
+++ JavaScript/Fundamentals - Part 2/MainTask/funcDeclVSExpression.js
@@ -36,3 +36,23 @@ LECTURE: Arrow Functions
 const percentageOfWorld3 = currPopulation => currPopulation / 7900000000 * 100;
 const currPopulationPercentage3 = percentageOfWorld3(1441000000);
 console.log(`${currPopulationPercentage3.toFixed(1)}%`);
+
+/*
+LECTURE: Functions Calling Other Functions
+    1. Create a function called 'describePopulation'. Use the function type you
+like the most. This function takes in two arguments: 'country' and
+'population', and returns a string like this: 'China has 1441 million people,
+which is about 18.2% of the world.'
+
+    2. To calculate the percentage, 'describePopulation' call the
+'percentageOfWorld1' you created earlier
+
+    3. Call 'describePopulation' with data for 3 countries of your choice
+*/
+
+function describePopulation(country, population) {
+    const averagePopulationPercentage = percentageOfWorld3(population);
+    return `${country} has ${population / 1000000} million people, which is about ${averagePopulationPercentage.toFixed(1)}% of the world.`
+}
+
+console.log(describePopulation("China", 1441000000));
