81dc3880809fac9eddfb6791a68d53c168cf033e
Davide Todorov <davide.todorov@abv.bg>
2020-12-28 08:29:59 +0200

    Finished Coding Challenge #2

---
+++ JavaScript/Fundamentals - Part 2/Coding Challenges/CodingChallenge#2/script.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/*
+Coding Challenge #2
+
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+
+Your tasks:
+    1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+    2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+    3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+    4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+
+Test data: 125, 555 and 44
+*/
+
+const calculateTip = function (bill) {
+    return (bill >= 50 && bill <= 300) ? bill * 0.15 : bill * 0.2;
+}
+
+const billsArr = new Array(125, 555, 44);
+
+
+const tipsArr = new Array(billsArr.length);
+for (let i = 0; i < tipsArr.length; i++) {
+    tipsArr[i] = calculateTip(billsArr[i]);
+}
+
+
+const totalArr = new Array(billsArr.length);
+for (let i = 0; i < tipsArr.length; i++) {
+    totalArr[i] = billsArr[i] + tipsArr[i];
+}
+
+console.log(billsArr);
+console.log(tipsArr);
+console.log(totalArr);
