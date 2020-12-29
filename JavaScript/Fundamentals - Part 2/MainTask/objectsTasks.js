a1055920392477686f976584ac242aad949d17c4
Davide Todorov <davide.todorov@abv.bg>
2020-12-29 18:18:05 +0200

    Finished introduction to objest task

---
+++ JavaScript/Fundamentals - Part 2/MainTask/objectsTasks.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+LECTURE: Introduction to Objects
+
+1. Create an object called 'myCountry' for a country of your choice, containing
+properties 'country', 'capital', 'language', 'population' and
+'neighbours' (an array like we used in previous assignments)
+*/
+
+const myCountry = {
+    country: "Bulgaria",
+    capital: "Sofia",
+    language: "Bulgarian",
+    population: 6500000
+};
