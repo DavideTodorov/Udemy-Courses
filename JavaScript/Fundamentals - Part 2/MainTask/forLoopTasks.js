81e9279da17c2f1e2783cb8a5adb584a9b0bbc79
Davide Todorov <davide.todorov@abv.bg>
2020-12-30 13:30:44 +0200

    Finished arrayLooping Task

--- JavaScript/Fundamentals - Part 2/MainTask/forLoopTasks.js
+++ JavaScript/Fundamentals - Part 2/MainTask/forLoopTasks.js
@@ -11,3 +11,36 @@ the console (for numbers 1 to 50): `Voter number 1 is currently voting`
 for (let voter = 1; voter <= 50; voter++) {
     console.log(`Voter number ${voter} is currently voting`);
 }
+
+
+/*
+LECTURE: Looping Arrays, Breaking and Continuing
+
+    1. Let's bring back the 'populations' array from a previous assignment
+
+    2. Use a for loop to compute an array called 'percentages2' containing the
+percentages of the world population for the 4 population values. Use the
+function 'percentageOfWorld1' that you created earlier
+
+    3. Confirm that 'percentages2' contains exactly the same values as the
+'percentages' array that we created manually in the previous assignment,
+and reflect on how much better this solution is
+*/
+
+const populations = new Array(1441000000, 13000000, 7000000, 33000000);
+
+const calcPopulationPercentages = function (currPopulation) {
+    return currPopulation / 7900000000 * 100;
+}
+
+const percentages = new Array(populations.length);
+
+for (let i = 0; i < populations.length; i++) {
+    percentages[i] = calcPopulationPercentages(populations[i]);
+}
+
+console.log(percentages);
+
+if(percentages.length === populations.length){
+    console.log("Both arrays have the same length!")
+}
