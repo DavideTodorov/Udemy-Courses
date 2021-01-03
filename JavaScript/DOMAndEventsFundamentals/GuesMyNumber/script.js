df7dd7a961041c341ef42010380fcb8eb9042534
Davide Todorov <davide.todorov@abv.bg>
2021-01-03 17:58:55 +0200

    Add eventListener

--- JavaScript/DOMAndEventsFundamentals/GuesMyNumber/script.js
+++ JavaScript/DOMAndEventsFundamentals/GuesMyNumber/script.js
@@ -1,1 +1,9 @@
-'use strict';
+"use strict";
+
+document.querySelector(".check").addEventListener("click", function () {
+  const enteredNum = Number(document.querySelector(".guess").value);
+
+  if (!enteredNum) {
+    document.querySelector(".message").textContent = "No number entered!";
+  }
+});
