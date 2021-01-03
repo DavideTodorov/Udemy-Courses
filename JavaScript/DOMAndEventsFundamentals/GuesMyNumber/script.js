dacb53d0fa651255dd1c45547bebb62967765aae
Davide Todorov <davide.todorov@abv.bg>
2021-01-03 19:01:25 +0200

    implement most of game logic

--- JavaScript/DOMAndEventsFundamentals/GuesMyNumber/script.js
+++ JavaScript/DOMAndEventsFundamentals/GuesMyNumber/script.js
@@ -1,9 +1,33 @@
 "use strict";
 
+const numToGuess = Math.trunc(Math.random() * 20) + 1;
+
+let currScore = Number(document.querySelector(".score").textContent);
+
+//todo: remove
+document.querySelector(".number").textContent = numToGuess;
+
 document.querySelector(".check").addEventListener("click", function () {
   const enteredNum = Number(document.querySelector(".guess").value);
 
-  if (!enteredNum) {
-    document.querySelector(".message").textContent = "No number entered!";
+  if (enteredNum < 1 || enteredNum > 20) {
+    document.querySelector(".message").textContent =
+      "Enter number between 1 and 20!";
+  } else if (enteredNum === numToGuess) {
+    document.querySelector(".message").textContent = "Correct number!";
+  } else if (enteredNum > numToGuess) {
+    document.querySelector(".message").textContent = "Too high!";
+    currScore--;
+  } else if (enteredNum < numToGuess) {
+    document.querySelector(".message").textContent = "Too low!";
+    currScore--;
   }
+
+  if (currScore <= 0) {
+    document.querySelector(".message").textContent =
+      "Your score is 0! You lost! Press \"Again\" to play again!";
+      document.querySelector(".check").disabled = true
+  }
+
+  document.querySelector(".score").textContent = currScore;
 });
