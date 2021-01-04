89762c242d78c0328c4cd63bb295572e4b08fad4
Davide Todorov <davide.todorov@abv.bg>
2021-01-04 07:14:50 +0200

    Manipulate body if the game is won

--- JavaScript/DOMAndEventsFundamentals/GuesMyNumber/script.js
+++ JavaScript/DOMAndEventsFundamentals/GuesMyNumber/script.js
@@ -10,23 +10,38 @@ document.querySelector(".number").textContent = numToGuess;
 document.querySelector(".check").addEventListener("click", function () {
   const enteredNum = Number(document.querySelector(".guess").value);
 
+  //If input is invalid or empty
   if (enteredNum < 1 || enteredNum > 20) {
     document.querySelector(".message").textContent =
       "Enter number between 1 and 20!";
+
+    //If there is a win condition
   } else if (enteredNum === numToGuess) {
     document.querySelector(".message").textContent = "Correct number!";
+
+    document.querySelector("body").style.backgroundColor = 
+    "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
+
+
+    //If entered num is greater than the num to guess
   } else if (enteredNum > numToGuess) {
     document.querySelector(".message").textContent = "Too high!";
     currScore--;
+
+    //If entered num is lower than the num to guess
   } else if (enteredNum < numToGuess) {
     document.querySelector(".message").textContent = "Too low!";
     currScore--;
   }
 
+
+  //If your score goes to 0
   if (currScore <= 0) {
     document.querySelector(".message").textContent =
-      "Your score is 0! You lost! Press \"Again\" to play again!";
-      document.querySelector(".check").disabled = true
+      'Your score is 0! You lost! Press "Again" to play again!';
+    document.querySelector(".check").disabled = true;
   }
 
   document.querySelector(".score").textContent = currScore;
