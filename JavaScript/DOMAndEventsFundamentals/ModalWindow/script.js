b03a829beb611fddf83895c92c527b98b247c4f4
Davide Todorov <davide.todorov@abv.bg>
2021-01-04 18:32:13 +0200

    Get fields

--- JavaScript/DOMAndEventsFundamentals/ModalWindow/script.js
+++ JavaScript/DOMAndEventsFundamentals/ModalWindow/script.js
@@ -1,1 +1,6 @@
-'use strict';
+"use strict";
+
+const buttonsShowModal = document.querySelectorAll(".show-modal");
+const modal = document.querySelector(".modal");
+const buttonCloseModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
