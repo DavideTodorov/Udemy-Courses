852de071c0a63f9f69455f07a6bbfbc90acc74e4
Davide Todorov <davide.todorov@abv.bg>
2021-01-12 17:22:51 +0200

    Finished bind method practise

--- JavaScript/Functions/MainTask/script.js
+++ JavaScript/Functions/MainTask/script.js
@@ -36,12 +36,50 @@ const eurowings = {
   bookings: [],
 };
 
+//Call method
 book.call(lufthansa, 123, "George");
 book.call(lufthansa, 123, "David");
-book.call(lufthansa, [333, "John"]);
+book.call(lufthansa, 333, "John");
 console.log(lufthansa);
 
+//Apply method
 book.apply(eurowings, [443, "Michael"]);
 book.apply(eurowings, [673, "Jonas"]);
 book.apply(eurowings, [673, "Bobo"]);
 console.log(eurowings);
+
+//Bind method
+const bookLH = book.bind(lufthansa);
+bookLH("1234", "Martin");
+
+//With event listener
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
+
+//Partial application
+//With Bind
+const addTax = function (tax, value) {
+  return value + value * tax;
+};
+
+const addVAT = addTax.bind(null, 0.2);
+console.log(addVAT(100));
+
+
+// //Function returns function
+//
+// const addTax = function (tax) {
+//   return function (value) {
+//     return value + value * tax;
+//   };
+// };
+//
+// const addVAT = addTax(0.2);
+// console.log(addVAT(100));
