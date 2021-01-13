6bc99d25e3d53ca7389c2e5193a7ac1554773622
Davide Todorov <davide.todorov@abv.bg>
2021-01-13 07:30:41 +0200

    Finished array methods practice

---
+++ JavaScript/Arrays/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// const currencies = new Map([
+//     ['USD', 'United States dollar'],
+//     ['EUR', 'Euro'],
+//     ['GBP', 'Pound sterling'],
+//   ]);
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+let arr = ["a", "b", "c", "d", "e"];
+let arr2 = ["j", "i", "h", "g", "f"];
+
+//Slice method
+console.log(arr.slice(1, 3));
+console.log(arr);
+
+//Splice method
+console.log(arr.splice(0, 2));
+console.log(arr);
+
+//Reverse method
+console.log(arr2.reverse());
+console.log(arr2);
+
+//Concat method
+const allLeters = arr.concat(arr2);
+console.log(allLeters);
+
+//Join method
+console.log(allLeters.join(", "));
