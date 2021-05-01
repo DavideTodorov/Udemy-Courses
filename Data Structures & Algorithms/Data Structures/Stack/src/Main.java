133d9916a49eaa8c285eb55df3b80501d568456b
Davide Todorov <davide.todorov@abv.bg>
2021-05-01 17:54:57 +0300

    Finished Stack

--- Data Structures & Algorithms/Data Structures/Stack/src/Main.java
+++ Data Structures & Algorithms/Data Structures/Stack/src/Main.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Stack<String> stack = new Stack<>(T, );
+        Scanner scanner = new Scanner(System.in);
+
+        Stack<Integer> numbersStack = new Stack<>(5);
+        numbersStack.push(5);
+        numbersStack.push(4);
+        numbersStack.push(3);
+        numbersStack.push(2);
+        numbersStack.push(1);
+        
+        System.out.println(numbersStack.isEmpty());
+
+        while (!numbersStack.isEmpty()) {
+            System.out.println(numbersStack.pop());
+        }
+
+        System.out.println(numbersStack.isEmpty());
     }
 }
--- Data Structures & Algorithms/Data Structures/Stack/src/Stack.java
+++ Data Structures & Algorithms/Data Structures/Stack/src/Stack.java
@@ -1,34 +1,46 @@
-import java.lang.reflect.Array;
-
 public class Stack<T> {
 
     private int maxSize;
     private T[] stackArray;
     private int topIndex;
 
+    //Initialise the Stack
     public Stack(int maxSize) {
         this.maxSize = maxSize;
         this.stackArray = (T[]) new Object[maxSize];
         this.topIndex = -1;
     }
 
-    public void push() {
-        //TODO
+    public void push(T element) {
+        //Check if the array has reached it's max size
+        if (topIndex >= maxSize - 1) {
+            throw new IllegalStateException("You have reached max capacity of " + maxSize + "!");
+        }
+
+        //Add the element
+        topIndex++;
+        stackArray[topIndex] = element;
     }
 
     public T pop() {
-        //TODO
+        //Check if there are any present elements
+        if (topIndex < 0) {
+            throw new IllegalStateException("You have no elements in the stack!");
+        }
 
-        return null;
+        //Remove and return the top element
+        T oldElement = stackArray[topIndex];
+        topIndex--;
+        return oldElement;
     }
 
     public T peek() {
-        //TODO
-        return null;
+        //Return the top element
+        return stackArray[topIndex];
     }
 
     public boolean isEmpty() {
-        //TODO
-        return true;
+        //Check if there are any elements in the array
+        return topIndex < 0;
     }
 }
