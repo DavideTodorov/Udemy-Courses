dcfaf94a559e7b0d7780f85dfc9cb84557948181
Davide Todorov <davide.todorov@abv.bg>
2021-05-07 18:58:27 +0300

    Add comments to the RecursiveBinarySearch

--- Data Structures & Algorithms/Algorithms/RecursiveBinarySearch/src/Main.java
+++ Data Structures & Algorithms/Algorithms/RecursiveBinarySearch/src/Main.java
@@ -6,20 +6,27 @@ public class Main {
         //Expected output: 6
     }
 
-    public static int recursiveBinarySearch(int[] arr, int el, int startIndex, int endIndex) {
+    public static int recursiveBinarySearch(int[] arr, int element, int startIndex, int endIndex) {
         int midIndex = (startIndex + endIndex) / 2;
 
         if (startIndex > endIndex) {
             //The end of the array is reached and the items hasn't been found so the
             //method returns -1
             return -1;
-        } else if (arr[midIndex] == el) {
+
+        } else if (arr[midIndex] == element) {
             //The searched element is found on the mid index and we retutn it
             return midIndex;
-        } else if (arr[midIndex] < el) {
-            return recursiveBinarySearch(arr, el, midIndex + 1, endIndex);
+
+        } else if (arr[midIndex] < element) {
+            //The searched element is greater than the middle element so the start index becomes the
+            //index after the midIndex
+            return recursiveBinarySearch(arr, element, midIndex + 1, endIndex);
+            
         } else {
-            return recursiveBinarySearch(arr, el, startIndex, midIndex - 1);
+            //The searched element is less than the middle element so the end index becomes the
+            //index before the midIndex
+            return recursiveBinarySearch(arr, element, startIndex, midIndex - 1);
         }
     }
 }
